8e1976e345692f2a875d0a49a343968dfbbd2527
Max Snippe <38857185+MaxSnippe@users.noreply.github.com>
2021-10-01 20:58:11 +0200

    Updated to Saxion green

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,6 +57,10 @@ autodoc_typehints = "description"
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'style_nav_header_background': '#009c82'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
